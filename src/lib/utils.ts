484dcbc9c56b8771e8565259bf3cb97b5b63ff1c
Leo Kuo <p900372012@gmail.com>
2024-04-25 23:43:29 +0800

    chore: remove useless packages

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,4 @@
-import type { ClassValue } from "clsx";
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
 import { ColorCode } from "~/constants/colors";
-
-export const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
-};
 
 export const hexToDecimal = (colorCode: ColorCode) => {
   const hex = colorCode.substring(1);
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import path from "path";
 import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
-// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [
-    /*
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
+  plugins: [],
   server: {
     port: 3000,
   },
